5a4c0b69af3ba04e0d3cffb4de866ef1d805eed3
patrikhuang <patrikhuang@patrikhuangdeMacBook-Pro.local>
2017-01-07 01:00:26 +0800

    create project

---
+++ PHSort/PHSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHSort
+//
+//  Created by patrikhuang on 2017/1/7.
+//  Copyright © 2017年 patrikhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHSort/PHSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHSort
+//
+//  Created by patrikhuang on 2017/1/7.
+//  Copyright © 2017年 patrikhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
